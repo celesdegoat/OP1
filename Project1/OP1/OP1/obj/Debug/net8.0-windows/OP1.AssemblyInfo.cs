f4c20f2d94a62f85a7bce24ad9301d23b3b0cf41
Niek <PS248772@edu.summacollege.nl>
2024-05-17 12:16:45 +0200

    I just made a calander

--- Project1/OP1/OP1/obj/Debug/net8.0-windows/OP1.AssemblyInfo.cs
+++ Project1/OP1/OP1/obj/Debug/net8.0-windows/OP1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b059a3f552e11d794c856d4fe53c731a0a543655")]
 [assembly: System.Reflection.AssemblyProductAttribute("OP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
